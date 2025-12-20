6f86221f32b3c423ea8dd9de9623162d0af6c803
Abdurakhim <esenkanov_ab@iuca.kg>
2025-12-20 18:15:36 +0600

    config: Add MEDIA settings and serve media in DEBUG

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -58,6 +58,10 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
